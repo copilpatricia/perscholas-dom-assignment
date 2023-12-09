d237567cbf1e1d04a856f6244ed759c9243ee59a
Patricia Muresan <patriciamariamrsn@gmail.com>
2023-12-08 20:45:08 -0500

    update

--- SBA DOM/script.js
+++ SBA DOM/script.js
@@ -1,3 +1,36 @@
-const navbar = document.getElementById("nav-id")
+const navbar = document.getElementById("nav-id");
 const links = document.querySelectorAll("a");
-console.log(links);
+
+links.forEach((link, i) => {
+        if(i === 0){
+            link.setAttribute("href", "#about")
+            console.log(link);
+        }else if(i === 1) {
+            link.setAttribute("href", "#signup")
+            console.log(link);
+        }else if(i === 2){
+            link.setAttribute("href", "#contact")
+            console.log(link);
+        }
+})
+
+const main = document.createElement("main");
+document.body.appendChild(main)
+const title = document.createElement("h1")
+main.appendChild(title)
+title.textContent = "Merry Christmas!";
+
+title.addEventListener("click", ()=> {
+    if(title.textContent === "Merry Christmas!"){
+        title.textContent = "Feliz navidad!"
+        title.classList.add("title-spanish")
+    }else if(title.textContent === "Feliz navidad!"){
+        title.textContent = "Merry Christmas!"
+        title.classList.remove("title-spanish")
+    }
+})
+
+const img = document.createElement("img");
+main.appendChild(img);
+
+console.log(title.nextElementSibling)
